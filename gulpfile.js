c78dee0bb8737d0b6dc4062d20a0fa678c8ecaa6
zhouyg <zhou.y.g.890@gmail.com>
2017-07-11 23:53:38 +0800

    rebuild

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const clean = require('gulp-clean');
 
-gulp.task('default', () => {
+gulp.task('clean', () => {
+  gulp.src('./bin/*.js').pipe(clean());
+});
+
+gulp.task('default', ['clean'], () => {
 
   gulp.src('./src/**/**.js').pipe(babel({
     "presets":["es2015"],
